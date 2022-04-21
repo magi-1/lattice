5ef6456c56cd90f6d79c59987763386a83e27857
magi-1 <mechior.magi@gmail.com>
2022-04-21 18:16:50 -0500

    add base investor files

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
 # https://opensource.com/article/18/8/what-how-makefile
 
-pull_all_data:
-	@pdm run python $(CURDIR)/lattice/utils/get_historical_data.py
+pull_all:
+	@pdm run python $(CURDIR)/lattice/utils/get_historical.py
 	
--- lattice/utils/get_historical.py
+++ lattice/utils/get_historical.py
@@ -9,16 +9,18 @@ from pyarrow import json
 from pathlib import Path
 from dotenv import load_dotenv
 from lattice.exchanges.ftx_client import FtxClient
+import lattice.utils.paths as paths
 load_dotenv()
 
 
 END_DATE = datetime.datetime.fromisoformat('2022-04-21')
+MAX_CALLS = 1500
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument(
-        "--resolution", type=int, default=60, help="Window length in seconds"
+        "--resolution", type=int, default=900, help="Window length in seconds"
         )
     parser.add_argument(
         "--days", default=365, type=int, help="Start and end time"
@@ -36,8 +38,9 @@ if __name__ == "__main__":
     start_date = END_DATE - datetime.timedelta(days=days)
     t0, t1 = start_date.timestamp(), END_DATE.timestamp()
     num_obs = (t1-t0)/resolution
-    num_chunks = int(np.ceil(num_obs/1500))+1
+    num_chunks = int(np.ceil(num_obs/MAX_CALLS))+1
     time_bounds = np.linspace(t0, t1, num_chunks)
+
     markets = pd.read_csv('lattice/data/markets.csv')
     for market in markets['name']:
         print(f'Pulling last {days} days of {market}...')
@@ -53,7 +56,7 @@ if __name__ == "__main__":
             dfs.append(_df)
 
         data = pd.concat(dfs)
-        dirname = Path(f'lattice/data/{days}_days_{resolution}_seconds/')
+        dirname = paths.data/f'{days}_days_{resolution}_seconds'
         dirname.mkdir(parents=True, exist_ok=True)
         fname = market.replace('/','_')
         fpath = dirname/(f'{fname}.parquet')
